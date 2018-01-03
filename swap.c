b23b4cdb12180dcf2e6626d1956c164c82aa1bb2
Pritesh Ranjan <pranjan341@gmail.com>
2018-01-04 01:37:49 +0530

    completed swaping with three different methods

---
+++ swap.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+void swap(int* a,int* b)
+{
+    int *tmp;
+    *tmp=*a;
+    *a=*b;
+    *b=*tmp;
+}
+void main()
+{
+    int a, b;
+    printf("Enter the first number:\n");
+    scanf("%d",&a);
+    printf("Enter second number:\n");
+    scanf("%d", &b);
+   /*
+   a=a+b;
+   b=a-b;
+   a=a-b;
+   */
+   a=a^b;
+   b=a^b;
+   a=a^b;
+   //swap(&a, &b);
+   printf("Thus after swapping:\n");
+   printf("First number\tSecond number\n");
+   printf("%d\t\t%d\n", a, b);
+}
